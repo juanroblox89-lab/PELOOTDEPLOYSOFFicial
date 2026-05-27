5c09c3d1a128da3a26d8d2e0443afcbe38e57373
juanroblox89-lab <juanroblox89@gmail.com>
2026-05-26 22:14:29 -0500

    fix: change authDomain to pelootdata.firebaseapp.com to fix Google OAuth redirect URI mismatch

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -10,7 +10,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-aut
 
 const firebaseConfig = {
   apiKey: "AIzaSyCkxXf93VlfWb9F6MtYk0FYpPFoBMMqBGA",
-  authDomain: "www.peloot.shop",
+  authDomain: "pelootdata.firebaseapp.com",
   projectId: "pelootdata",
   storageBucket: "pelootdata.firebasestorage.app",
   messagingSenderId: "1037452690351",
